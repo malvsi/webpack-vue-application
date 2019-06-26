d653ef8c81e846bb707c3cbe685da6f73b7fda36
malvsi <3555419453@qq.com>
2019-06-26 15:03:33 +0800

    some change

--- src/App.vue
+++ src/App.vue
@@ -3,32 +3,32 @@
     <mt-header fixed title="Webpack·Vue项目"></mt-header>
 
     <div class="app-container">
-	<router-view></router-view>
-
-        </div>
+      <router-view></router-view>
+    </div>
 
     <nav class="mui-bar mui-bar-tab">
-		<router-link to="/home" class="mui-tab-item mui-active" href="#tabbar">
-				<span class="mui-icon mui-icon-home"></span>
-				<span class="mui-tab-label">首页</span>
-			</router-link>
+      <router-link to="/home" class="mui-tab-item mui-active active">
+        <span class="mui-icon mui-icon-home"></span>
+        <span class="mui-tab-label">首页</span>
+      </router-link>
 
-		<router-link to="/user" class="mui-tab-item" href="#tabbar-with-chat">
-				<span class="mui-icon mui-icon-contact"></span>
-				<span class="mui-tab-label">会员</span>
-			</router-link>
+      <router-link to="/member" class="mui-tab-item" >
+        <span class="mui-icon mui-icon-contact"></span>
+        <span class="mui-tab-label">会员</span>
+      </router-link>
 
-		<router-link to="/cart" class="mui-tab-item" href="#tabbar-with-contact">
-				<span class="mui-icon mui-icon-extra mui-icon-extra-cart"><span class="mui-badge">0</span></span>
-				<span class="mui-tab-label">购物车</span>
-			</router-link>
+      <router-link to="/cart" class="mui-tab-item">
+        <span class="mui-icon mui-icon-extra mui-icon-extra-cart">
+          <span class="mui-badge">0</span>
+        </span>
+        <span class="mui-tab-label">购物车</span>
+      </router-link>
 
-		<router-link to="/search" class="mui-tab-item" href="#tabbar-with-map">
-				<span class="mui-icon mui-icon-search"></span>
-				<span class="mui-tab-label">搜索</span>
-			</router-link>
-	</nav>
-
+      <router-link to="/search" class="mui-tab-item">
+        <span class="mui-icon mui-icon-search"></span>
+        <span class="mui-tab-label">搜索</span>
+      </router-link>
+    </nav>
   </div>
 </template>
 
@@ -37,8 +37,7 @@ export default {};
 </script>
 
 <style scoped>
-    .app-container{
-        margin-top:40px;
-    }
-
+.app-container {
+  margin-top: 40px;
+}
 </style>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import VueRouter from 'vue-router';
 import home from './components/Home.vue';
-import user from './components/User.vue';
+import member from './components/Member.vue';
 import cart from './components/Cart.vue';
 import search from './components/Search.vue';
 
@@ -9,7 +9,7 @@ var router = new VueRouter({
     routes: [
         {path: '/', redirect: '/home'},
         {path: '/home', component:home},
-        {path: '/user', component:user},
+        {path: '/user', component:member},
         {path: '/cart', component:cart},
         {path: '/search', component:search},
     ]
