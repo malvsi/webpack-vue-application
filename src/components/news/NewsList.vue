184f7f7e2dd3d8cefd42032206247eb486a522cb
malvsi <3555419453@qq.com>
2019-06-28 11:41:14 +0800

    getting and rendering newsList

--- src/components/news/NewsList.vue
+++ src/components/news/NewsList.vue
@@ -1,16 +1,102 @@
 <template>
-    <div>
-        <h1>新闻列表</h1>
-    </div>
+  <div>
+    <ul
+      class="mui-table-view mui-table-view-chevron"
+      v-infinite-scroll="loadMore"
+	  infinite-scroll-disabled="loading"
+      infinite-scroll-distance="10"
+    >
+      <li class="mui-table-view-cell mui-media" v-for="item in newsList" :key="item.docid">
+        <a class="mui-navigate-right">
+          <img class="mui-media-object mui-pull-left" :src="item.imgsrc">
+          <div class="mui-media-body">
+            <h1>{{ item.title }}</h1>
+            <p class="mui-ellipsis">
+              <span>发表时间：{{ item.mtime }}</span>
+              <span>点击：{{ item.votecount | formatNum }}次</span>
+            </p>
+          </div>
+        </a>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {
+import Vue from "vue";
+import VueResrouce from "vue-resource";
+import { Totas } from "mint-ui";
+import { constants } from 'crypto';
 
-}
+Vue.use(VueResrouce);
+
+export default {
+  data() {
+    return {
+	  newsList: [],
+	  previousPage: 0,
+	  nextPage: 20,
+	  loading: false,
+    };
+  },
+  created() {
+    this.getNewsList();
+  },
+  filters: {
+	  formatNum(data) {
+		  var datacopy = data.toString();
+		  var len = datacopy.length;
+		  if(len == 2 ) {
+			  datacopy = '00' + datacopy;
+		  }else if(len == 3) {
+			  datacopy = "0" + datacopy;
+		  }else if(len == 1) {
+			  datacopy = '000' + datacopy;
+		  }
+		  return datacopy;
+	  }
+  },
+  methods: {
+    getNewsList() {
+      this.$http
+        .get(
+          "http://c.3g.163.com/nc/article/list/T1467284926140/" + this.previousPage + "-" + this.nextPage + ".html"
+        )
+        .then(result => {
+		  this.newsList = this.newsList.concat(result.body.T1467284926140);
+        })
+        .catch(err => {
+          Totas("加载失败： " + err);
+        });
+    },
+    loadMore() {
+		console.log(this.loading);
+		this.loading = true;
+
+		this.nextPage = this.previousPage;
+		this.previousPage += 20;
+		this.getNewsList();
+		console.log(this.newsList);
+
+		this.loading = false;
+	}
+  }
+};
 </script>
 
 
 <style lang="less" scoped>
-
+.mui-table-view {
+  li {
+    h1 {
+      font-size: 14px;
+    }
+    .mui-ellipsis {
+      color: darkturquoise;
+      font-size: 12px;
+      display: flex;
+      justify-content: space-between;
+    }
+  }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@
 import Vue from 'vue';
 import app from './App.vue';
 import './lib/mui-master/dist/css/mui.css';
-import { Header } from 'mint-ui';
 import 'mint-ui/lib/style.css';
 import './lib/mui-master/examples/hello-mui/css/icons-extra.css';
 import './lib/mui-master/examples/hello-mui/fonts/mui-icons-extra.ttf';
@@ -10,15 +9,16 @@ import VueRouter from 'vue-router';
 import router from './router.js';
 import VueResource from 'vue-resource';
 
-import { Swipe, SwipeItem } from 'mint-ui';
+import { Swipe, SwipeItem, Header, InfiniteScroll } from 'mint-ui';
 
-Vue.component(VueResource);
+Vue.component(Header.name, Header);
 Vue.component(Swipe.name, Swipe);
 Vue.component(SwipeItem.name, SwipeItem);
 
 Vue.use(VueRouter);
+Vue.use(InfiniteScroll);
+Vue.component(VueResource);
 
-Vue.component(Header.name, Header);
 
 var vm = new Vue({
     el: '#app',
